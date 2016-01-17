1e76cbd7b0e0fb1ad4e28cb6c6da48c3f260f480
Jacob Glueck <swimgiraffe435@gmail.com>
2016-01-17 12:34:10 -0500

    Fix cancel bug in change description.

--- src/grow/action/ChangeDescription.java
+++ src/grow/action/ChangeDescription.java
@@ -8,7 +8,7 @@ import grow.Scene;
 
 /**
  * Represents: an action that changes the description of a scene
- * 
+ *
  * @author Jacob Glueck
  *
  */
@@ -16,17 +16,15 @@ public class ChangeDescription extends Action {
 
 	@Override
 	public Scene act(Scene current, Game world, Scanner input, PrintStream output) {
-		output.printf("The current description for the scene \"%s\" is: %s", current.name(), current.description());
-		output.println();
-		output.println("What would you like the new description to be?");
-		String description = "";
-		while (description.length() == 0) {
-			description = input.nextLine();
-		}
-		current.setDescription(description);
-		output.println("Description set.");
-		// Re-enter the room with the new description
-		return new Go(current.name()).act(current, world, input, output);
+		return Util.handleCancel(current, output, () -> {
+			output.printf("The current description for the scene \"%s\" is: %s", current.name(), current.description());
+			output.println();
+			String description = Util.read(output, input, "What would you like the new description to be?", "Bad description", (s) -> s);
+			current.setDescription(description);
+			output.println("Description set.");
+			// Re-enter the room with the new description
+			return new Go(current.name()).act(current, world, input, output);
+		});
 	}
 
 	@Override
